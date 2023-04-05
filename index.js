bcfc1460dacb912b067c5322c5682952702cc9a3
Nehir Gencay <nhrgncy@gmail.com>
2023-04-05 15:24:06 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+/*
+const mondayWork = function(activity="go to the office"){
+    return `This Monday, I will ${activity}.`;
+};
+mondayWork("work from home");
+*/
+
+function saturdayFun(activity="roller-skate"){
+  return `This Saturday, I want to ${activity}!`;
+}
+function mondayWork(activity="go to the office"){
+  return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(flair="*"){
+  return function(word="special"){
+    return `You are ${flair}${word}${flair}!`;
+  };
+}
+const encouragingFunction= wrapAdjective("!!!");
+
+
+
+
